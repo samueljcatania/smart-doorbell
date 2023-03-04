c7415db5ca12815f77be2ad9d9c7334c273aa14f
Samuel <samueljcatania@gmail.com>
2023-03-04 15:59:24 -0500

    Created C++ Project within Repo

---
+++ Hello.cpp
@@ -0,0 +1,10 @@
+//
+// Created by samue on 2023-03-04.
+//
+
+#include <iostream>
+#include "Hello.h"
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+}
---
+++ Hello.h
@@ -0,0 +1,14 @@
+//
+// Created by samue on 2023-03-04.
+//
+
+#ifndef INC_33307B_GROUP_17_PROJECT_HELLO_H
+#define INC_33307B_GROUP_17_PROJECT_HELLO_H
+
+
+class Hello {
+
+};
+
+
+#endif //INC_33307B_GROUP_17_PROJECT_HELLO_H
